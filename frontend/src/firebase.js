32227a79d1e10107cf187912bc6d134791ca4591
r-nilesh-c <nileshrn01@gmail.com>
2026-01-07 20:40:25 +0530

    Fix Vercel deployment config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
+//fixed
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
